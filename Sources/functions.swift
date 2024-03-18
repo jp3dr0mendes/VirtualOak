382e34b2b1955e1f95b5f91f5a6fe53eecf8d9af
pedro-honorio <joao.pedro.pkgo732@gmail.com>
2024-03-18 08:57:55 -0300

    fixing

--- Sources/functions.swift
+++ Sources/functions.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
-struct Status{
+
+struct Status: Encodable, Decodable {
     var fome: Int = 100
     var saude: Int = 100
     var felicidade: Int = 100
 }
 
-struct Buddy {
+struct Buddy: Encodable, Decodable {
     var nomeBuddy: String? = nil
     let tipo: String
     let especie: String
@@ -34,7 +35,7 @@ var squirtle: Buddy = Buddy(tipo: "Agua", especie: "squirtle")
 
 var bulbasaur: Buddy = Buddy(tipo: "Planta", especie: "bulbasaur")
 
-let pokemons:[Buddy] = [pikachu, charmander, squirtle, bulbasaur]
+var pokemons:[Buddy] = [pikachu, charmander, squirtle, bulbasaur]
 
 func showPokemons(buddy: Int){
     switch buddy{
@@ -45,10 +46,14 @@ func showPokemons(buddy: Int){
     }
 }
 
-func criarBasedeDados(conteudo: String, filePath: String) {
+
+
+
+func criarBasedeDados(buddy: Buddy, filePath: String) {
     let fileManager = FileManager.default
     
-    if let data = conteudo.data(using: .utf8) {
+    if let data = ("\(buddy)").data(using: .utf8) {
+//    if let data = conteudo.data(using: .utf8) {
         let success = fileManager.createFile(atPath: filePath, contents: data, attributes: nil)
         if success {
             print("File created and data written successfully.")
@@ -60,26 +65,174 @@ func criarBasedeDados(conteudo: String, filePath: String) {
     }
 }
 
-// Chamando a função para criar um arquivo de texto
-
-/*
-
-func criarBancoDeDados(data: Buddy, filePath: String) {
-    let jdonEncoder = JSONEncoder()
-
-    jsonEncoder.outputFormatting = .prettyPrinted
-
-    do {
-        let jsonData = try jsonEncoder.encode(data)
-
-        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let fileURL = documentsURL.appendingPathComponent(filePath)
-
-        try jsonData.write(to: fileURL)
-        print("Sua jornada começou!!")
-    } catch {
-        print("Error!")
+func checarBasedeDados() {
+    let fileManager = FileManager.default
+    //reads data from "myfile.txt" to show that data has been written
+    if let fileData = fileManager.contents(atPath: filePath) {
+        if let fileContentString = String(data: fileData, encoding: .utf8) {
+            print("File contents:")
+            print(fileContentString)
+        }
+    } else {
+        print("Failed to read file")
     }
 }
 
-*/
+// Função auxiliar: Limpa o terminal
+func clearTerminalScreen() {
+    let clear = Process()
+    clear.launchPath = "/usr/bin/clear"
+    clear.arguments = []
+    clear.launch()
+    clear.waitUntilExit()
+}
+
+
+// Chamando a função para criar um arquivo de texto
+
+//func criarBancoDeDados(data: Buddy, filePath: String) {
+//    let jsonEncoder = JSONEncoder()
+//
+//    jsonEncoder.OutputFormatting = .prettyPrinted//.prettyPrinted
+//
+//    do {
+//        let jsonData = try JSONEncoder.encode(data)//JSONEncoder.encode(data)
+//
+//        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//        let fileURL = documentsURL.appendingPathComponent(filePath)
+//
+//        try jsonData.write(to: fileURL)
+//        print("Sua jornada começou!!")
+//    } catch {
+//        print("Error!")
+//    }
+//}
+
+func criarBancoDeDados(data: Buddy){
+    
+    let file: String = "pokeData.json"
+    let jsonEncode = JSONEncoder()
+    let structToJSON = try! jsonEncode.encode(data)
+    
+    
+    if let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                        in: .userDomainMask).first {
+        let pathWithFilename = documentDirectory.appendingPathComponent(file)
+        do {
+            try structToJSON.write(to: pathWithFilename)
+            print("Pokedex criada")
+        } catch {
+            print("Error in save JSON")
+        }
+    }
+}
+//let path = "~/Users/user/Documents/"
+let path: String = "User/user/VirtualOak"
+
+func lerJSON(_ path: String) -> String{
+    if let fileURL = Bundle.main.url(forResource: "User/user/VirtualOak", withExtension: "json") {
+        do {
+            // Lê o conteúdo do arquivo como uma string
+            let jsonString = try String(contentsOf: fileURL)
+            print("Conteúdo do arquivo JSON como string:")
+            return jsonString
+        } catch {
+            print("Erro ao ler o arquivo JSON como string: \(error.localizedDescription)")
+        }
+    } else {
+        print("Arquivo JSON não encontrado.")
+    }
+    return ""
+}
+
+//func lerBancodeDados() -> Buddy {
+//    let buddyData = Data(lerJSON(path).utf8)
+//    
+//    print(lerJSON(path))
+//    
+//    let jsonDecoder = JSONDecoder()
+//    
+//    let testBuddy = Buddy(nomeBuddy: "ljdfng", tipo: "kxhdbf", especie: "ksjdhfjks")
+//    
+//    jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+//    
+//    do {
+//        let decodedBuddy = try jsonDecoder.decode(Buddy.self, from: buddyData)
+//        
+//        print("""
+//        Pokemon Status: \(decodedBuddy.status.felicidade)
+//        """)
+//        return decodedBuddy
+//    } catch {
+//        print("Error: \(error.localizedDescription)")
+//    }
+//    
+//    return testBuddy
+//}
+
+let fileURL = URL(fileURLWithPath: "/Users/user/pokeData.json")
+
+func saveData(_ path:URL, buddy: Buddy){
+    // Instância da struct
+
+    // Crie um objeto JSONEncoder
+    let encoder = JSONEncoder()
+
+    // Codificar a struct para JSON Data
+    do {
+        let jsonData = try encoder.encode(buddy)
+
+        // Converta JSON Data em String (opcional)
+        if let jsonString = String(data: jsonData, encoding: .utf8) {
+
+            // Gravar dados JSON no arquivo
+            try jsonData.write(to: path)
+
+        }
+    } catch {
+        print("Erro ao codificar a struct para JSON: \(error)")
+    }
+}
+
+
+func readData(_ path:URL){
+    do {
+        // Ler os dados JSON do arquivo
+        let jsonData = try Data(contentsOf: path)
+
+        // Crie um objeto JSONDecoder
+        let decoder = JSONDecoder()
+
+        // Decodificar os dados JSON em uma instância da struct Person
+        let person = try decoder.decode(Buddy.self, from: jsonData)
+
+        // Imprimir os dados da pessoa
+        print("Nome: \(person.especie)")
+
+    } catch {
+        print("Erro ao ler o arquivo JSON: \(error)")
+    }
+}
+
+
+//func lerBancodeDados() -> Buddy? {
+//    let jsonDecoder = JSONDecoder()
+//    
+//    // Verifique se o arquivo pokeData.json existe no diretório de documentos
+//    if let filePath = Bundle.main.path(forResource: "/Users/user/Documents/.pokeData", ofType: "json"){
+//        do {
+//            let buddyData = try Data(contentsOf: URL(fileURLWithPath: Pa))
+//            let decodedBuddy = try jsonDecoder.decode(Buddy.self, from: buddyData)
+//            
+//            print("""
+//            Pokemon Status: \(decodedBuddy.status.felicidade)
+//            """)
+//            return decodedBuddy
+//        } catch {
+//            print("Error: \(error.localizedDescription)")
+//            return nil
+//        }
+//
+//    }
+
+    
--- Sources/virtualOak.swift
+++ Sources/virtualOak.swift
@@ -38,9 +38,9 @@ struct Init: ParsableCommand {
     //TODO: manipulacao de arquivo
     
     func run(){
-//        let fileURL = URL(fileURLWithPath: "/Users/user/Documents/")
-//        let contentDirectory = try! FileManager.default.contentsOfDirectory(at: fileURL, includingPropertiesForKeys: nil)
-//        let validateArray = contentDirectory.map{$0.absoluteString}
+        //let fileURL = URL(fileURLWithPath: "/Users/user/Documents/")
+        //let contentDirectory = try! FileManager.default.contentsOfDirectory(at: fileURL, includingPropertiesForKeys: nil)
+        //let validateArray = contentDirectory.map{$0.absoluteString}
         
         
         let fileManager = FileManager.default
@@ -53,7 +53,7 @@ struct Init: ParsableCommand {
             
             return
         }
-        
+//        
         
         print("Saudações Treinador!\n")
         
@@ -70,19 +70,57 @@ struct Init: ParsableCommand {
             Pressione enter para continuar...
             """)
         
-        let _continueText = readLine() ?? ""
+        let continueText = readLine() ?? ""
         
-        print(shell("clear"))
+        clearTerminalScreen()
         
         pokebals()
 
         let chooseBuddy = obterBuddyValido()
         
-        print("Você escolheu o Pokemon: " + pokemons[chooseBuddy].especie)
+        clearTerminalScreen()
+        
+        print("Você escolheu o Pokemon: " + pokemons[chooseBuddy].especie.capitalized)
         showPokemons(buddy: chooseBuddy)
         
+        print("Você deseja colocar um apelido no seu Pokemon? [S/N]")
+        while (true){
+            let selectNickname = readLine() ?? "N"
+            
+            if selectNickname.uppercased() == "N"{
+                
+                pokemons[chooseBuddy].nomeBuddy = pokemons[chooseBuddy].especie
+                
+                print("Nice! Dados de \(pokemons[chooseBuddy].nomeBuddy?.capitalized ?? "") adicionados na PokeDex")
+                
+                break
+                
+            }
+            
+            else if selectNickname.uppercased() == "S"{
+                
+                print("Qual o nome voce deseja colocar no seu Buddy? ")
+                
+                let nickname = readLine()
+                
+                pokemons[chooseBuddy].nomeBuddy = nickname
+                
+                print("Nice! Dados de \(pokemons[chooseBuddy].nomeBuddy?.capitalized ?? "") adicionados na PokeDex")
+                
+                break
+                
+            }
+            else {
+                print("Entrada invalida")
+                print("Digite SIM (S) ou NAO (N)")
+            }
+        }
         
-        criarBasedeDados(conteudo: String(chooseBuddy), filePath: filePath)
+//        criarBancoDeDados(data: pokemons[chooseBuddy])
+        saveData(fileURL, buddy: pokemons[chooseBuddy])
+        readData(fileURL)
+//        criarBasedeDados(buddy: pokemons[chooseBuddy], filePath: filePath)
+//        checarBasedeDados()
         
         //        if let url = URL(string: pokeapi+chooseBuddy){
         //
@@ -126,32 +164,24 @@ struct Init: ParsableCommand {
         //            print("erro aqui oh")
         //        }
         //    }
-        //}
+        //}        
+    }
+}
+
+struct Check{
+    
+    
+    
+    func run(){
         
-        struct Care{
-            @Option(help: "Formas de se cuidar o seu pokemon")
-            var careOption: String
-            
-            func run(){
-                
-            }
-        }
+    }
+}
+
+struct Care{
+    @Option(help: "Formas de se cuidar o seu pokemon")
+    var careOption: String
+    
+    func run(){
         
-        @discardableResult
-        func shell(_ command: String) -> String {
-            let task = Process()
-            let pipe = Pipe()
-            
-            task.standardOutput = pipe
-            task.arguments = ["-c", command]
-            task.launchPath = "/bin/zsh"
-            task.launch()
-            
-            let data = pipe.fileHandleForReading.readDataToEndOfFile()
-            let output = String(data: data, encoding: .utf8)!
-            
-            print(output)
-            return output
-        }
     }
 }
